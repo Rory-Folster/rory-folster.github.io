4ba832e03d0324f855e44b378b849709285b66c4
Rory-Folster <61396000+Rory-Folster@users.noreply.github.com>
2022-05-09 20:38:31 +1200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# Personal Website
+Starting a personal website project, this will allow me to build my skills and have something to show to possible future employers. 
---
+++ index.js
@@ -0,0 +1,16 @@
+// for(let rep = 1; rep <= 10; rep++) {
+//     console.log(`Yeah Yeah ${rep}`)
+// }
+
+// const jonasArray = [
+//     'Jonas',
+//     'Schemdtmann',
+//     2037 - 1991,
+//     'teacher',
+//     ['Michael', 'Peter', 'Steven']
+// ];
+
+// for(let i = 0; i < jonasArray.length; i++) {
+//     console.log(jonasArray[i]);
+// }
+
